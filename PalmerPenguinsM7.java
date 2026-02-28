4da3311ea50c27dc7845ada58c2265345d5c77c6
briannageigerstudent <s3841751@students.fscj.edu>
2026-02-28 02:30:31 -0500

    Add files via upload

--- PalmerPenguinsM7.java
+++ PalmerPenguinsM7.java
@@ -1,15 +1,16 @@
 // PalmerPenguinsM7.java
-//
-//
+// Brianna Geiger
+// 2/28/26
 // Refactored Palmer Penguins analysis using object-oriented approach
 
 public class PalmerPenguinsM7 {
     public static void main(String[] args) {
         // Create an instance of SpeciesCounter
+        SpeciesCounter counter = new SpeciesCounter();
         
 
         // Read species data
-        
+        counter.readSpeciesData();
 
         // Initialize species count
         counter.initializeSpeciesCount();
